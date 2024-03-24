5b5ec467fe31c92bae9d25007869fc73d8d01e8f
Natalia Demidenko <n.demidenko@cti.ru>
2024-03-24 18:11:13 +0300

    check stickers

---
+++ checkSticker.rb
@@ -0,0 +1,49 @@
+require "rspec"
+require "selenium-webdriver"
+require "pry"
+
+def checkStickersMostPopular()
+  elements = @driver.find_elements(:xpath, "//*[@id='box-most-popular']/div/ul/li")
+  for element in elements
+    if element.find_elements(:css, "div.sticker").size() != 1
+      puts("Sticker error in Most Popular category!")
+    end
+  end
+  puts ("Stickers in Most Popular category - ok ")
+end
+
+def checkStickersCampaigns()
+  elements = @driver.find_elements(:xpath, "//*[@id='box-campaigns']/div/ul/li")
+  for element in elements
+    if element.find_elements(:css, "div.sticker").size() != 1
+      puts("Sticker error in Campaigns category!")
+    end
+  end
+  puts ("Stickers in Campaigns category - ok ")
+end
+
+def checkStickersLatestProducts()
+  elements = @driver.find_elements(:xpath, "//*[@id='box-latest-products']/div/ul/li")
+  for element in elements
+    if element.find_elements(:css, "div.sticker").size() != 1
+      puts("Sticker error in Latest Products category!")
+    end
+  end
+  puts ("Stickers in Latest Products - ok ")
+end
+
+begin
+  @driver = Selenium::WebDriver.for(:chrome)
+  @wait = Selenium::WebDriver::Wait.new(timeout: 20)
+
+  @driver.get "http://localhost/litecart/"
+
+  checkStickersMostPopular()
+  checkStickersCampaigns()
+  checkStickersLatestProducts()
+
+
+  @driver.close()
+  puts ("✔ Test OK")
+end
+return
